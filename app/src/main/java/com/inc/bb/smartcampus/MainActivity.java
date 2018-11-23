4a72ca66d1ef81cde59092ed68c4ebfadc09da49
JimBartels <37241404+JimBartels@users.noreply.github.com>
2018-11-23 23:39:53 +0100

    Commenting, tidying and changes to Mainactivity
    Comments have been added to functions, certain sections in functions and for variables. Moreover MainActivity has been tidied up.
    
    Lastly, a change has been made, a local User class has been used to be on standby whenever connection has been lost. In this way whenever connection is lost the local stored username and password of the user will be used to re-login.

--- app/src/main/java/com/inc/bb/smartcampus/MainActivity.java
+++ app/src/main/java/com/inc/bb/smartcampus/MainActivity.java
@@ -26,72 +26,78 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity  {
 
-    //Required for setting up the connection with the Database
+    //Required for setting up the connection with the Database and authentication client
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
-   //Required for sending data to Server
+    //Globalizing of layout elements (initializing)
     EditText mNameField;
     EditText mPassField;
     Button mFirebaseBtn;
-    private FirebaseAuth mAuth;
+    Drawable buttondrawable1;
+    Drawable buttondrawable2;
+
+    //Used for logging purposes
     String TAG;
-    boolean hasAccount;
+
+    //User credentials and user class for re-login purposes
     String longitude="0";
     String latitude= "0";
     String userId = "";
-    Drawable buttondrawable1;
-    Drawable buttondrawable2;
-    Boolean isTextChanged1;
-    Boolean isTextChanged2;
     private String name;
     private String password;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase initialization and authentication client
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         mDatabase = database.getReference();
         mAuth = FirebaseAuth.getInstance();
 
-              buttondrawable1 = ContextCompat.getDrawable(getApplicationContext(),R.drawable.buttonshape);
-        buttondrawable2 = ContextCompat.getDrawable(getApplicationContext(),R.drawable.buttonshapebefore);
-
+        //Login activity linking of widget variables to physical layout elements
+        buttondrawable1 = ContextCompat.getDrawable(getApplicationContext(),R.drawable.buttonshape);
+        buttondrawable2 = ContextCompat.getDrawable(getApplicationContext()
+                ,R.drawable.buttonshapebefore);
         mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
         mNameField = (EditText) findViewById(R.id.name_field);
         mPassField = (EditText) findViewById(R.id.pass_field);
         mFirebaseBtn.setBackground(buttondrawable2);
-        final FirebaseUser user =mAuth.getCurrentUser();
         mFirebaseBtn.setText("Login");
-        buttonchanger();
+
+        //Layout listeners for above widgets, these listeners get called whenver login/register
+        // is initiated
+        editTextsListeners();
+        loginButtonListener();
+    }
+
+    private void loginButtonListener() {
         mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name = mNameField.getText().toString().trim();
                 password = mPassField.getText().toString().trim();
 
-                if(true){
-                    if(name!=null && !name.isEmpty()&& !password.isEmpty() /*&& name.charAt(0)=='s'&& name.length()== 7 */){
-                        name=name+"@random.com";
-                        register(name,password);
-                }
-                else if (name.charAt(0)!='s' || name.length()!=7){ //Hier pak ik aan dat het moet beginnen met een s en 7 lang moet zijn
+                if(name!=null && !name.isEmpty()&& !password.isEmpty()){
+                    name=name+"@random.com";
+                    register(name,password);}
+                    /*else if (name.charAt(0)!='s' || name.length()!=7)
                         if(password==null || password.isEmpty()){
                             mNameField.setError("Invalid studentnumber");
                             mPassField.setError("Please fill in password");
                         }
                         else{mNameField.setError("Invalid studentnumber");}
-                    }
-                }
-
-                /*HashMap<String,String> dataMap = new HashMap<String,String>();
-                dataMap.put("Studentnumber",name);
-                dataMap.put("Password",email);*/
-
-
+                    }*/
+                // The if else statement was used in the past for detection of studentnumber for
+                // logging in. This was left out for testing convenience.
             }
         });
-    }
+    } // Listener for the login button. If password and
+    // username are not empty it goes to register function.
+
 
 
     public void register(final String studentNumber, final String pass) {
@@ -99,31 +105,42 @@ public class MainActivity extends AppCompatActivity  {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // task.isSuccessful means if the creating of a new user was successful
                         if(task.isSuccessful()){
                             Toast toast = Toast.makeText(getApplicationContext(), "Registration successful", Toast.LENGTH_SHORT);
                             toast.show();
-                            // Hier gaat die heen als de user nog niet geregistreerd is. Is die wel geregistreerd dan is de task niet succesfull, dan krijg je een exception togestuurd. Deze pak ik hieronder aan!
                             FirebaseUser user = mAuth.getCurrentUser();
                             userId=studentNumber.replace("@random.com", "");
                             updateDisplayName(user);
                             writeNewUser(userId,studentNumber, pass, longitude,latitude);
-                            updateUI(user);
+                            checkUserLoggedInAndGPSActivityIntent(user);
                         }
-                        else if(!task.isSuccessful())//Task is niet succesfull exception toegestuurd
-                        {String temp= "";
+
+                        // !task.isSuccessful means that creating a new user was not succesful and
+                        // exception is sent back (reason why) to the app
+                        else if(!task.isSuccessful())
+                        {
+                            String temp= "";
                             try {
-                            temp =  task.getException().getMessage();
-                        } catch (Exception e) {}
-                        //Hieronder als d e execption is dat de user al bestaat, dan probeer die in te loggen met de opgegeven password en naam.
+                                temp =  task.getException().getMessage();
+                            } catch (Exception e) {}
+
+                            //If the exception is case of user already existing
                             if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                                 Toast.makeText(getApplicationContext(),"Logging in",Toast.LENGTH_SHORT).show();
                                 login(studentNumber,pass);
-                            }else{Toast.makeText(MainActivity.this, temp,
-                                    Toast.LENGTH_SHORT).show();}} //ALs de exception dus iets anders is dan krijg je hem gwn op je scherm te zienw at je fout doet. Bvb een te kort password
+                            }
 
+                            //Else it is some other error and that is shown to the user
+                            else{Toast.makeText(MainActivity.this, temp,
+                                    Toast.LENGTH_SHORT).show();}}
                     }
                 });
-    }
+    } // Tries to register
+    // by the "studentnumber" variable and "pass variable", these are the password and username
+    // inputs from the edittexts as seen in the listeners. If register is successful -> Gpsactivity,
+    // if not exception by firebase is handled by either logging in or showing error to user by toast.
 
     private void updateDisplayName(FirebaseUser user) {
 
@@ -150,12 +167,16 @@ public class MainActivity extends AppCompatActivity  {
         mAuth.signInWithEmailAndPassword(name,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
+                //Handles if logging in is succesful
                 if(task.isSuccessful()){
                     Toast toast = Toast.makeText(getApplicationContext(), "Log in successful", Toast.LENGTH_SHORT);
                     toast.show();
                     FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
+                    checkUserLoggedInAndGPSActivityIntent(user);
                 }
+
+                //Handles exception by firebase and shows to user
                 else if(!task.isSuccessful())
                 {String temp= "";
                     try {
@@ -166,45 +187,49 @@ public class MainActivity extends AppCompatActivity  {
                 }
             }
         });
-    }
+    } //Tries to login to firebase auth client,
+    // if successful -> GPSactivity, if not error is shown.
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, " On start");
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);}
+        checkUserLoggedInAndGPSActivityIntent(currentUser);}
 
-    private void updateUI(FirebaseUser currentUser) {
-        if(currentUser!=null ){
-            //De app kijkt altijd eerst naar onstart na dat de onclicklistener is neergezet. Als dus de Currentuser niet null is dan gaat die gelijk door naar de andere activity omdat je al ingelogd bent.Hier kom je vanuit Onstart, vanuit register en login methods.
+    private void checkUserLoggedInAndGPSActivityIntent(FirebaseUser currentUser) {
+        if(currentUser!=null){
             Intent intentgps=new Intent(this, GpsActivity.class);
             intentgps.putExtra("userId",currentUser.getEmail().replace("@random.com",""));
             intentgps.putExtra(" password",password);
             startActivity(intentgps);
         }
+
         if (currentUser==null){
+            if(user!=null){
+                login(user.studentnumber,user.password);}
+        }
+    } //Checks whether user is logged in and if so goes to GPS activity.
 
-
-
-        }}
-    private void writeNewUser(String userId, String studentNumber, String passWord, String Longitude, String Latitude){
+    private void writeNewUser(String userId, String studentNumber, String passWord, String Longitude
+            , String Latitude){
         userId=userId.replace("@random.com","");
-        studentNumber = studentNumber.replace("@random.com","");
-        User user = new User(studentNumber,passWord, latitude, longitude);
+        user = new User(studentNumber,passWord, latitude, longitude);
         mDatabase.child("users").child(userId).setValue(user);
         return;
+    } //Writes a User class (nothing to do with firebase), this is
+    // used for staying logged in to firebase this will prevent logging in again after short connection
+    // loss
 
-    }
 
-    private void buttonchanger() {
+    private void editTextsListeners() {
         mPassField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
 
-            @Override
+            @Override //Sets button to the nice colour after both fields have been filled in
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if(mPassField.getText().toString().length()>0){
                     if(mNameField.getText().toString().length()>0){
@@ -246,17 +271,9 @@ public class MainActivity extends AppCompatActivity  {
 
             }
         });
-    }
 
-    private boolean isStudentnumer() {
-        if(mNameField.getText().toString().length()==7){
-           String check = mNameField.getText().toString().trim();
-            if(check.charAt(0)=='s') {
-                return true;
-            }else return false;
-        }
-        else return false;
-    }
+    } //Listener for user input into username and password editTexts, changes button to other color
+    // when those two both have input, void function.
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
